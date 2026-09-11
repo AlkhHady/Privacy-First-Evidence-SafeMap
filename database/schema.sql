cdcd38112d124a91ca966c9448ed6dd5e3056da4
AlkhHady <pikoryu08@gmail.com>
2026-09-11 14:09:48 +0700

    add framework

--- database/schema.sql
+++ database/schema.sql
@@ -487,5 +487,10 @@ commit;
 revoke execute on function public.handle_new_user()
 from public, anon, authenticated;
 
-revoke execute on function public.rls_auto_enable()
-from public, anon, authenticated;
+do $
+begin
+    if to_regprocedure('public.rls_auto_enable()') is not null then
+        execute 'revoke execute on function public.rls_auto_enable() from public, anon, authenticated';
+    end if;
+end
+$;
