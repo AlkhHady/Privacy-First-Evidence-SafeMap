86c251eb0f8ca1449f01624d0af1c4a1e212c4df
arsyi-naya <nazhayaarsyifa@gmail.com>
2026-09-11 14:47:22 +0700

    Add files via upload

---
+++ backend/riskZones.js
@@ -0,0 +1,12 @@
+const riskZones = [
+  {
+    wilayah: "Padang Pasir",
+    jumlahKasus: 12,
+    latitude: -0.9451,
+    longitude: 100.3544,
+    radius: 800,
+    zona: "merah"
+  }
+];
+
+module.exports = riskZones;
